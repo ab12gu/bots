489ba983fe09f24c9e94d1748f7795c5802b8b58
Abhay Gupta <ab18gu@gmail.com>
2025-07-27 14:25:45 -0700

    Create slack_post.py for automating polls onto bike polo newbie night

---
+++ slack_post.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+SLACK_TOKEN = os.getenv("SLACK_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+text = "*Wednesday newbie polo 6pm till dark @ Judkin's Park*\nReact with 🚴 if you're in!"
+
+res = requests.post("https://slack.com/api/chat.postMessage", json={
+    "channel": CHANNEL_ID,
+    "text": text
+}, headers={
+    "Authorization": f"Bearer {SLACK_TOKEN}",
+    "Content-Type": "application/json"
+})
+
+print("Posted:", res.json())
