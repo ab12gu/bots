ca21d15581f679170d0a41088fa30d3bd2b44af7
Abhay Gupta <ab18gu@gmail.com>
2025-10-02 10:52:57 -0700

    Update great_games_today.py change slacktoken to slackbottoken

--- slack/206bikepolo/great_games_today.py
+++ slack/206bikepolo/great_games_today.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-SLACK_TOKEN = os.getenv("SLACK_TOKEN")
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 text = (
@@ -11,7 +11,7 @@ res = requests.post("https://slack.com/api/chat.postMessage", json={
     "channel": CHANNEL_ID,
     "text": text
 }, headers={
-    "Authorization": f"Bearer {SLACK_TOKEN}",
+    "Authorization": f"Bearer {SLACK_BOT_TOKEN}",
     "Content-Type": "application/json"
 })
 
