76ea9c35905cb360f919b0db8a3c74a919e22b8b
Abhay Gupta <ab18gu@gmail.com>
2025-10-24 15:23:16 -0700

    Test just printgn my user in slack bot

--- slack/byobg/scripts/invite_dm.py
+++ slack/byobg/scripts/invite_dm.py
@@ -25,9 +25,8 @@ for user in users:
 
    
     # Check each user individually
-    for uid in [user_id, "U06AWV00DL2"]:
-        info = client.users_info(user=uid)
-        print(uid, info["user"]["deleted"], info["user"]["is_bot"])
+    info = client.users_info(user="U06AWV00DL2")
+    print("U06AWV00DL2", info["user"]["deleted"], info["user"]["is_bot"])
 
 
     # Load message from file
